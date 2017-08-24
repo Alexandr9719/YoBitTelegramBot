00149e7565006823e98816730b6788190979b08f
Alexander <sanchello2009@gmail.com>
2017-08-25 02:55:55 +0300

    Checking for invalid currencies has been added

--- YoBitApi.js
+++ YoBitApi.js
@@ -3,76 +3,79 @@ var rp = require('request-promise');
 var cron = require('cron').CronJob;
 
 function YoBitApi() {
-	this.info_uri = 'https://yobit.net/api/3/info/';
-	this.depth_uri = 'https://yobit.net/api/3/depth/';
-	this.ticker_uri = 'https://yobit.net/api/3/ticker/';
-	this.trades_uri = 'https://yobit.net/api/3/trades/';
+    this.info_uri = 'https://yobit.net/api/3/info/';
+    this.depth_uri = 'https://yobit.net/api/3/depth/';
+    this.ticker_uri = 'https://yobit.net/api/3/ticker/';
+    this.trades_uri = 'https://yobit.net/api/3/trades/';
 };
 exports.YoBitApi = YoBitApi;
-YoBitApi.prototype.ticker = async function(currency, callback) {
-	if (!currency) {callback("wrong input")}
-	var options = {
-		uri: this.ticker_uri+currency,
+YoBitApi.prototype.ticker = async function(currency, callback, failure) {
+    var options = {
+        uri: this.ticker_uri + currency,
 
-		headers:{
-			'Content-Type': 'application/json'
-		},
+        headers: {
+            'Content-Type': 'application/json'
+        },
 
-		json: true
-	}
-	console.log(options.uri);
-	await rp(options)
-		.then(function(response) {
-			if(response.success == 0){throw "Bad request";}
-			callback(response);
-		})
-		.catch(function(err) {
-			console.log(err);
-			throw err;
-		})
+        json: true
+    }
+    console.log("ticker uri: " + options.uri);
+    await rp(options)
+        .then(function(response) {
+            callback(response);
+        }, function(err) { console.log(err); failure(err)});
 };
 YoBitApi.prototype.info = async function(callback) {
-	var options = {
-		uri: this.info_uri,
-		headers:{
-			'Content-Type': 'application/json'
-		},
-		json: true
-	}
-	await rp(options)
-		.then(function(response) {
-			callback(response);
-		})
-		.catch(function(err) {
-			throw err;
-		})
+    var options = {
+        uri: this.info_uri,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        json: true
+    }
+    await rp(options)
+        .then(function(response) {
+            callback(response);
+        })
+        .catch(function(err) {
+            throw err;
+        })
 };
-YoBitApi.prototype.check_valid = async function(currency_list, callback) {
-	var self = this;
-	async function query(local_currency) { //replace func -> promise
-		var options = {
-			uri: this.ticker_uri + currency_list,
-			headers:{
-				'Content-Type': 'application/json'
-			},
-			json: true
-		}	
-	await rp(options)
-		.then(function(response) {
-			if (response.success == 0) {
-				console.log(response);
-				var invalid_pair = response.error.substring(response.error.indexOf(':')).substring(2);
-				//console.log("invalid pair: " + response.error.substring(response.error.indexOf(':')).substring(2).length);
-				//console.log("Index item: " + currency_list.indexOf(response.error.substring(response.error.indexOf(':')).substring(2).trim()));
-				currency_list = currency_list.replace(invalid_pair, '');
-				if (currency_list[0] ==  '-' || currency_list[currency_list.length-1] == '-')
-					currency_list = currency_list.replace('-', ' ').trim().replace(' ', '-');				
-				console.log("new query string " + currency_list);
-				self.check_valid(currency_list);
-			}
-			else{
-				callback(currency_list);
-			}
-		}, function(err) {callback('Wrong query');});
-	}	
+YoBitApi.prototype.check_valid = async function(currency_list, callback, failure) {
+    var self = this;
+    async function query(currency) {
+        if (!currency) { console.log('repair all invalid_pair');
+            failure('All currency pairs are incorrect'); } else {
+            var options = {
+                uri: self.ticker_uri + currency,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                json: true
+            }
+            console.log("check_valid uri: " + options.uri);
+            await rp(options)
+                .then(function(response) {
+                    if (response.success == 0) {
+                        console.log(response);
+                        var invalid_pair = response.error.substring(response.error.indexOf(':')).substring(2);
+                        //console.log("invalid pair: " + response.error.substring(response.error.indexOf(':')).substring(2).length);
+                        //console.log("Index item: " + currency.indexOf(response.error.substring(response.error.indexOf(':')).substring(2).trim()));
+                        currency = currency.replace(invalid_pair, '');
+                        if (currency[0] == '-' || currency[currency.length - 1] == '-')
+                            currency = currency.replace('-', ' ').trim().replace(' ', '-');
+                        console.log("new query string " + currency);
+                        query(currency);
+                    } else {
+                        console.log('return: ' + Object.keys(response));
+                        console.log(currency);
+                        callback(currency);
+                    }
+                }, function(err) { console.log(err); failure(err); });
+        }
+    }
+    await query(currency_list);
+    // console.log('callback check_valid: ' + currency_list);
+    // process.exit();
+    // callback(currency_list);
 };
--- server.js
+++ server.js
@@ -1,127 +1,125 @@
 const TelegramBot = require('node-telegram-bot-api');
 const http = require('http');
-var {YoBitApi} = require('./YoBitApi');
-var {DB} = require('./DbConnection');
-var {MongoDriver} = require('./MongoDriver');
+var { YoBitApi } = require('./YoBitApi');
+var { DB } = require('./DbConnection');
+var { MongoDriver } = require('./MongoDriver');
 require('dotenv').config();
 // var async = require('asyncawait/async');
 // var await = require('asyncawait/await');
 const token = process.env.TELEGRAM_BOT_TOKEN;
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 var api = new YoBitApi();
-var db = new MongoDriver();;
+var db = new MongoDriver();
 
-var symbols = {
-	fav: "\u2764",
-	search: "\uD83D\uDD0D",
-	back: "\uD83D\uDD19"
-}
-
-bot.onText(/\/start/, (msg, match)=> {
-	try{
-		db.has_user(msg.from.id);
-		bot.sendMessage(msg.chat.id, "Please, select an option from the menu.");
-	}catch(e){
-		bot.sendMessage(msg.chat.id, "Sorry, some trouble with DataBase");
-	}
+bot.onText(/\/start/, (msg, match) => {
+    try {
+        db.has_user(msg.from.id);
+        bot.sendMessage(msg.chat.id, "Please, select an option from the menu.");
+    } catch (e) {
+        bot.sendMessage(msg.chat.id, "Sorry, some trouble with DataBase");
+    }
 });
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-  api.ticker(match[1], function(result) {
-  	console.log(result);
-  	bot.sendMessage(msg.chat.id, typeof result);
-  });
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+    api.ticker(match[1], function(result) {
+        console.log(result);
+        bot.sendMessage(msg.chat.id, typeof result);
+    });
 });
 
 bot.onText(/\/fav (.+)/, (msg, match) => {
-	console.log('favorites');
-	db.get_favorites(msg.from.id, function(res) {console.log(res[0]);});
+    console.log('favorites');
+    db.get_favorites(msg.from.id, function(res) { console.log(res[0]); });
 });
 
 bot.onText(/\/search (.+)/, (msg, match) => {
-	if (!match[1]) {
-		console.log(match);
-		bot.sendMessage(msg.chat.id, "Please, enter currency");
-	}
-	var currency = match[1];
-	var options = {
-		reply_markup: JSON.stringify({
-			inline_keyboard: [
-				[{text: 'Add to favorites', callback_data: 'add_fav'}, {text: 'Show data', callback_data: 'query_to_api'}],
-				[{text: 'Remove from favorites', callback_data: 'rm_fav'}]
-			]
-		})
-	};
-	bot.sendMessage(msg.chat.id, currency, options);
+    if (!match[1]) {
+        console.log(match);
+        bot.sendMessage(msg.chat.id, "Please, enter currency");
+    }
+    var currency = match[1];
+    var options = {
+        reply_markup: JSON.stringify({
+            inline_keyboard: [
+                [{ text: 'Add to favorites', callback_data: 'add_fav' },
+                    { text: 'Show data', callback_data: 'query_to_api' }
+                ],
+                [{ text: 'Remove from favorites', callback_data: 'rm_fav' }]
+            ]
+        })
+    };
+    bot.sendMessage(msg.chat.id, currency, options);
 });
 
-bot.on('callback_query', function (msg) {
-  //console.log(Object.keys(msg)); // msg.data refers to the callback_data
-  var currency = msg.message.text.toLowerCase().replaceAll(' ', '').replaceAll('/', '_').replaceAll(',', '-');
-  if (currency[0] == '-') {currency[0] == ''};
-  if (currency[currency.length-1] == '-') {currency[currency.length-1] == ''};
-  //console.log(currency);
-  var currency_list = [];
-  var temp = '';
-  for (var i = 0; i <= currency.length; i++) {
-  	if (currency[i] == '-' || currency[i] == undefined) {
-  		currency_list.push(temp);
-  		temp = '';
-  	}
-  	else{
-  		temp += currency[i];
-  	}
-  }
-  console.log(currency_list);
-  console.log(currency);
-  api.check_valid(currency, function(response) {
-  	currency_list = response;
-  	switch(msg.data){
-  	case 'query_to_api': 
-  		try{
-  			api.ticker(currency, function(result) {
-  		  			console.log(result);
-  		  			if (result.success === 0) {
-  		  				console.log('wrong query');
-  		  				bot.answerCallbackQuery(msg.id, "Wrong query", true);
-  		  			}
-  		  			else {
-  		  				var answer = '';
-  		  				for (var i = currency_list.length - 1; i >= 0; i--) {
-  		  					if (result[currency_list[i]]) {
-  		  						  answer += 'Currency: ' + currency_list[i].toUpperCase().replace('_', '/') + '\n' +
-  		  					      			'Last: ' + result[currency_list[i]].last + '\n' +
-  		  					           		'24High: ' + result[currency_list[i]].high + '\n'+
-  		  					           		'24Low: ' + result[currency_list[i]].low + '\n\n';
-  		  					}
-  		  				}
-  		  				bot.answerCallbackQuery(msg.id, answer, true).catch(function(err) {
-  		  					console.log(err.response.body.description);
-  		  					bot.answerCallbackQuery(msg.id, err.response.body.description, true);
-  		  				});
-  		  			}
-  		  		});
-  		}catch(e){bot.answerCallbackQuery(msg.id, "Sorry, some problem with YoBit service");}
-  		break;
-  	case 'add_fav':
-  		try{
-  			db.add_to_favorites(msg.from.id, currency_list);
-  		}catch(e){bot.answerCallbackQuery(msg.id, "Sorry, some problem with DataBase");}
-  		break;
-  	case 'rm_fav':
-  		try{
-  			db.delete_favorites(msg.from.id, currency_list, function() {});
-  		}catch(e){console.log(e.message);}
-  		break;
-  }
-  });
+bot.on('callback_query', function(msg) {
+    //console.log(Object.keys(msg)); // msg.data refers to the callback_data
+    var currency = msg.message.text.toLowerCase().replaceAll(' ', '')
+        .replaceAll('/', '_').replaceAll(',', '-');
+    if (currency[0] == '-') { currency[0] == '' };
+    if (currency[currency.length - 1] == '-') { currency[currency.length - 1] == '' };
+    //console.log(currency);
+    var currency_list = [];
+    var temp = '';
+    for (var i = 0; i <= currency.length; i++) {
+        if (currency[i] == '-' || currency[i] == undefined) {
+            currency_list.push(temp);
+            temp = '';
+        } else {
+            temp += currency[i];
+        }
+    }
+    console.log(currency_list);
+    console.log(currency);
+    api.check_valid(currency, function(response) {
+        console.log(response);
+        currency = response;
+        switch (msg.data) {
+            case 'query_to_api':
+                try {
+                    api.ticker(currency, function(result) {
+                        console.log(result);
+                        if (result.success === 0) {
+                            console.log('wrong query');
+                            bot.answerCallbackQuery(msg.id, "Wrong query", true);
+                        } else {
+                            var answer = '';
+                            for (var i = currency_list.length - 1; i >= 0; i--) {
+                                if (result[currency_list[i]]) {
+                                    answer += 'Currency: ' + currency_list[i].toUpperCase().replace('_', '/') + '\n' +
+                                        'Last: ' + result[currency_list[i]].last + '\n' +
+                                        '24High: ' + result[currency_list[i]].high + '\n' +
+                                        '24Low: ' + result[currency_list[i]].low + '\n\n';
+                                }
+                            }
+                            bot.answerCallbackQuery(msg.id, answer, true).catch(function(err) {
+                                console.log(err.response.body.description);
+                                bot.answerCallbackQuery(msg.id, err.response.body.description, true);
+                            });
+                        }
+                    }, function(err) {bot.answerCallbackQuery(msg.from.id, 'All currency pairs are incorrect', true);});
+                } catch (e) { bot.answerCallbackQuery(msg.id, "Sorry, some problem with YoBit service"); }
+                break;
+
+            case 'add_fav':
+                try {
+                    db.add_to_favorites(msg.from.id, currency_list);
+                } catch (e) { bot.answerCallbackQuery(msg.id, "Sorry, some problem with DataBase"); }
+                break;
+
+            case 'rm_fav':
+                try {
+                    db.delete_favorites(msg.from.id, currency_list, function() {});
+                } catch (e) { console.log(e.message); }
+                break;
+        }
+    }, function(err) {console.log("Failure: " + err); bot.answerCallbackQuery(msg.id, err);});
 });
 
 String.prototype.replaceAll = function(search, replacement) {
-	var target = this;
-	return target.split(search).join(replacement);
+    var target = this;
+    return target.split(search).join(replacement);
 };
