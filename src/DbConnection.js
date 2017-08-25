d5ad0a1af5d68200a0145d363c5dbf2647df0dc3
Alexander <sanchello2009@gmail.com>
2017-08-25 05:25:34 +0300

    Added support for node6

---
+++ src/DbConnection.js
@@ -0,0 +1,37 @@
+const lowdb = require('lowdb');
+const fileAsync = require('lowdb/lib/storages/file-async');
+
+function DB() {
+  this.db = lowdb('db.json', {
+    storage: fileAsync
+  });
+  if (!this.db.has('users').value()) {
+    this.db.set('users', []).write();
+  }
+}
+exports.DB = DB;
+DB.prototype.has_user = async function(user_id) {
+  const user = this.db.get('users').find({
+    id: user_id
+  }).value();
+  console.log(user);
+  if (user === undefined) {
+    await this.db.get('users')
+      .push({
+        'id': user_id
+      }).set('fav', {}).write();
+  }
+};
+DB.prototype.has_user_currency = function(user_id, currency) {
+
+};
+DB.prototype.add_currency = async function(user_id, currency) {
+  try {
+    var temp = await this.db.get('users').find({
+      id: user_id
+    }).value();
+    console.log(temp);
+  } catch (e) {
+    console.log(e.message);
+  }
+};
